6872a0ad407e33267e608fcc1ba171ebcec2c17a
UJJWAL SHUKLA <ujjwalshukla291@gmail.com>
2025-11-22 10:13:06 +0530

    Fix import statement for FeaturedProjects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Hero } from "@/components/sections/hero";
-import { FeaturedProjects } from "@/components/home/featured-projects";
+import FeaturedProjects from "@/components/home/featured-projects";
 import { SkillsShowcase } from "@/components/home/skills-showcase";
 import { Achievements } from "@/components/home/achievements";
 
